50ac8e87771e5262d2fff9b324357d6776a372d0
agautier <pillowin@gmx.com>
2021-11-15 20:45:18 +0100

    feat(ex01): done

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 14:14:31 by agautier          #+#    #+#             */
+/*   Updated: 2021/11/15 19:55:14 by agautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAP_TRAP_HPP
+#define CLAP_TRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+	public:
+		ClapTrap(std::string const& name = "Unnamed");
+		ClapTrap(ClapTrap const& ct);
+
+		virtual ~ClapTrap(void);
+
+		virtual void attack(std::string const& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		void equipWeapon(unsigned int amount);
+
+		std::string const& getName(void) const;
+		unsigned int getLifePoints(void) const;
+		unsigned int getHitPoints(void) const;
+		unsigned int getEnergyPoints(void) const;
+		unsigned int getAttackDamage(void) const;
+
+		ClapTrap& operator=(ClapTrap const& ct);
+
+	protected:
+		std::string _name;
+		unsigned int _life_points;
+		unsigned int _hit_points;
+		unsigned int _energy_points;
+		unsigned int _attack_damage;
+};
+
+std::ostream& operator<<(std::ostream& os, ClapTrap const& ct);
+
+#endif
+
---
+++ ex01/main.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 14:14:08 by agautier          #+#    #+#             */
+/*   Updated: 2021/11/15 20:32:59 by agautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include <cstdlib>
+
+int main(void) {
+	ClapTrap antoine("Antoine");
+	ClapTrap tonton("Tonton Xavier");
+
+	antoine.attack("Tonton Xavier");
+	std::cout << std::endl;
+	tonton.takeDamage(antoine.getAttackDamage());
+	std::cout << std::endl;
+
+	tonton.equipWeapon(9000);
+	std::cout << std::endl;
+	tonton.beRepaired(9000);
+	std::cout << std::endl;
+
+	antoine.equipWeapon(2);
+	std::cout << std::endl;
+
+	tonton.attack("Antoine");
+	std::cout << std::endl;
+	antoine.takeDamage(tonton.getAttackDamage());
+
+	std::cout << std::endl << " --------------------" << std::endl << std::endl;
+
+	ScavTrap oulet("Oulet");
+
+	oulet.attack("Antoine");
+	std::cout << std::endl;
+	antoine.takeDamage(oulet.getAttackDamage());
+	std::cout << std::endl;
+
+	oulet.equipWeapon(42);
+	std::cout << std::endl;
+
+	oulet.attack("Tonton Xavier");
+	std::cout << std::endl;
+	tonton.takeDamage(oulet.getAttackDamage());
+	std::cout << std::endl;
+
+	oulet.guardGate();
+
+	return (EXIT_SUCCESS);
+}
